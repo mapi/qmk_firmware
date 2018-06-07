0d9fec3e65ef4c681e4d432e87ca0e0c6a4a9410
mapi <mapi@okiizo.net>
2018-06-08 04:07:29 +0900

    右シフトキーを追加 未定義のキーは _______ ではなく XXXXXXX を使う

--- keyboards/xd75/keymaps/mapi/keymap.c
+++ keyboards/xd75/keymaps/mapi/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------|
  * | LCTRL   | A       | S       | D       | F       | G       | H       | J       | K       | l       | ; / +   | : / *   | ] / }   | ENTER   |         |
  * |---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------|
- * | LSHIFT  | Z       | X       | C       | V       | B       | N       | M       | , / <   | . / >   | / / ?   | \ / _   |         |         |         |
+ * | LSHIFT  | Z       | X       | C       | V       | B       | N       | M       | , / <   | . / >   | / / ?   | \ / _   | RSHIFT  |         |         |
  * |---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------|
  * | LCTRL   |         | LWIN    | LALT    | MUHENKAN| SPACE   | SPACE   | HENKAN  | KANA    | RALT    | APP     | RCTRL   |         |         |         |
  * '-----------------------------------------------------------------------------------------------------------------------------------------------------'
@@ -46,10 +46,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  [_QW] = { /* QWERTY */
   { KC_ZKHK,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  JIS_HAT,  JIS_JYEN, KC_BSPC  },
-  { KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     JIS_AT,   JIS_LBRC, _______,  _______  },
-  { KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  JIS_CLON, JIS_RBRC, KC_ENT,   _______  },
-  { KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  JIS_BSLS, _______,  _______,  _______  },
-  { KC_LCTL,  _______,  KC_LWIN,  KC_LALT,  KC_MHEN,  KC_SPC,   _______,  KC_HENK,  KC_KANA,  KC_RALT,  KC_APP,   KC_RCTL,  _______,  _______,  _______  },
+  { KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     JIS_AT,   JIS_LBRC, XXXXXXX,  XXXXXXX  },
+  { KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  JIS_CLON, JIS_RBRC, KC_ENT,   XXXXXXX  },
+  { KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  JIS_BSLS, KC_RSFT,  XXXXXXX,  XXXXXXX  },
+  { KC_LCTL,  XXXXXXX,  KC_LWIN,  KC_LALT,  KC_MHEN,  KC_SPC,   XXXXXXX,  KC_HENK,  KC_KANA,  KC_RALT,  KC_APP,   KC_RCTL,  XXXXXXX,  XXXXXXX,  XXXXXXX  },
  },
 
 /* FUNCTION
