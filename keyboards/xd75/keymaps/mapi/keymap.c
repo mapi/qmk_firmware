9df6c6b5f8533c7508b164a4d9310f95d72519a2
mapi <mapi@okiizo.net>
2018-06-08 04:22:26 +0900

    Add Backlight function layer.

--- keyboards/xd75/keymaps/mapi/keymap.c
+++ keyboards/xd75/keymaps/mapi/keymap.c
@@ -18,6 +18,7 @@
 // Layer shorthand
 #define _QW 0
 #define _FN 1
+#define _LED 2
 
 // for JIS Keyboard
 #define JIS_HAT  KC_EQL   // ^ and ~
@@ -40,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------|
  * | LSHIFT  | Z       | X       | C       | V       | B       | N       | M       | , / <   | . / >   | / / ?   | \ / _   | RSHIFT  |         |         |
  * |---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------|
- * | LCTRL   |         | LWIN    | LALT    | MUHENKAN| SPACE   | SPACE   | HENKAN  | KANA    | RALT    | APP     | RCTRL   |         |         |         |
+ * | LCTRL   |         | LWIN    | LALT    | MUHENKAN| SPACE   | SPACE   | HENKAN  | KANA    | RALT    | APP     | RCTRL   |         |         | FN(LED) |
  * '-----------------------------------------------------------------------------------------------------------------------------------------------------'
  */
 
@@ -49,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   { KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     JIS_AT,   JIS_LBRC, XXXXXXX,  XXXXXXX  },
   { KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  JIS_CLON, JIS_RBRC, KC_ENT,   XXXXXXX  },
   { KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  JIS_BSLS, KC_RSFT,  XXXXXXX,  XXXXXXX  },
-  { KC_LCTL,  XXXXXXX,  KC_LWIN,  KC_LALT,  KC_MHEN,  KC_SPC,   XXXXXXX,  KC_HENK,  KC_KANA,  KC_RALT,  KC_APP,   KC_RCTL,  XXXXXXX,  XXXXXXX,  XXXXXXX  },
+  { KC_LCTL,  XXXXXXX,  KC_LWIN,  KC_LALT,  KC_MHEN,  KC_SPC,   XXXXXXX,  KC_HENK,  KC_KANA,  KC_RALT,  KC_APP,   KC_RCTL,  XXXXXXX,  XXXXXXX,  MO(_LED) },
  },
 
 /* FUNCTION
@@ -65,13 +66,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |        |        | RGB TG | FN     | RGB RMD| RGB MD | P0     |        | P.     | PENT   | PENT   | FN     |        |        |        |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
- 
+
  [_FN] = { /* FUNCTION */
-  { KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_NLCK, KC_SLSH, KC_ASTR, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12   },
-  { KC_MSEL, KC_CALC, KC_MYCM, KC_MAIL, RGB_HUD, RGB_HUI, KC_P7,   KC_P8,   KC_P9,   KC_MINS, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS  },
-  { KC_MPRV, KC_MPLY, KC_MNXT, KC_MSTP, RGB_SAD, RGB_SAI, KC_P4,   KC_P5,   KC_P6,   KC_PLUS, _______, RESET,   _______, _______, _______  },
-  { KC_VOLD, KC_MUTE, KC_VOLU, KC_APP,  RGB_VAD, RGB_VAI, KC_P1,   KC_P2,   KC_P3,   KC_PENT, _______, _______, _______, _______, _______  },
-  { _______, _______, RGB_TOG, MO(_FN), RGB_RMOD,RGB_MOD, KC_P0,   _______, KC_PDOT, KC_PENT, KC_PENT, MO(_FN), _______, _______, _______  },
+  { XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX  },
+  { XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX  },
+  { XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX  },
+  { XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX  },
+  { XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX  },
+ },
+
+/* LED
+ * .-----------------------------------------------------------------------------------------------------------------------------------------------------.
+ * |         | BL_ON   | BL_OFF  |         |         |         |         |         |         |         |         |         |         |         |         |
+ * |---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------|
+ * |         | BL_INC  | BL_DEC  |         |         |         |         |         |         |         |         |         |         |         |         |
+ * |---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------|
+ * |         |         |         |         |         |         |         |         |         |         |         |         |         |         |         |
+ * |---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------|
+ * |         |         |         |         |         |         |         |         |         |         |         |         |         |         |         |
+ * |---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------|
+ * |         |         |         |         |         |         |         |         |         |         |         |         |         |         | FN(LED) |
+ * '-----------------------------------------------------------------------------------------------------------------------------------------------------'
+ */
+
+ [_LED] = { /* LED */
+  { XXXXXXX,  BL_ON,    BL_OFF,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX  },
+  { XXXXXXX,  BL_INC,   BL_DEC,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX  },
+  { XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX  },
+  { XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX  },
+  { XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, _______  },
  }
 };
 
