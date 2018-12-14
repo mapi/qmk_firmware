679f01533d024f2ed78385e0556478fcba167132
mapi <mapi@okiizo.net>
2018-12-15 08:02:11 +0900

    reconsidering new QWERTY layout.

--- keyboards/xd75/keymaps/mapi/keymap.c
+++ keyboards/xd75/keymaps/mapi/keymap.c
@@ -17,43 +17,32 @@
 #include "xd75.h"
 
 // Layer shorthand
-#define _QW 0
+#define QWERTY_LAYER 0
 #define _FN 1
 #define _LED 2
 
-// for JIS Keyboard
-#define JIS_HAT  KC_EQL   // ^ and ~
-#define JIS_JYEN KC_JYEN  // \ and |
-#define JIS_AT   KC_LBRC  // @ and `
-#define JIS_LBRC KC_RBRC  // [ and {
-#define JIS_CLON KC_QUOT  // : and *
-#define JIS_RBRC KC_BSLS  // ] and }
-#define JIS_BSLS KC_RO    // \ and _
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-/* QWERTY
- * .-----------------------------------------------------------------------------------------------------------------------------------------------------.
- * | E/J     | 1       | 2       | 3       | 4       | 5       | 6       | 7       | 8       | 9       | 0       | - / =   | ^ / ~   | \ / |   | BS      |
- * |---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------|
- * | TAB     | Q       | W       | E       | R       | T       | Y       | U       | I       | O       | P       | @ / `   | [ / {   |         |         |
- * |---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------|
- * | LCTRL   | A       | S       | D       | F       | G       | H       | J       | K       | l       | ; / +   | : / *   | ] / }   | ENTER   |         |
- * |---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------|
- * | LSHIFT  | Z       | X       | C       | V       | B       | N       | M       | , / <   | . / >   | / / ?   | \ / _   | RSHIFT  |         |         |
- * |---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------|
- * | LCTRL   |         | LWIN    | LALT    | MUHENKAN| SPACE   | MO(_FN) | HENKAN  | KANA    | RALT    | APP     | RCTRL   |         |         | FN(LED) |
- * '-----------------------------------------------------------------------------------------------------------------------------------------------------'
+/* QWERTY LAYER
+ *  .-----------------------------------------------------------------------------------------------------------------------------------------------------.
+ *  | E/J     | 1       | 2       | 3       | 4       | 5       |         | \ / |   | 6       | 7       | 8       | 9       | 0       | - / =   | ^ / ~   |
+ *  |---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------|
+ *  | TAB     | Q       | W       | E       | R       | T       |         |         | Y       | U       | I       | O       | P       | @ / `   | [ / {   |
+ *  |---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------|
+ *  | LCTRL   | A       | S       | D       | F       | G       |         | ESC     | H       | J       | K       | l       | ; / +   | : / *   | ] / }   |
+ *  |---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------|
+ *  | LSHIFT  | Z       | X       | C       | V       | B       |         |         | N       | M       | , / <   | . / >   | / / ?   | \ / _   | RSHIFT  |
+ *  |---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------|
+ *  | LCTRL   |         | LWIN    | LALT    | BS      | SPACE   | MHEN    | HENK    | ENTER   | MO      |         | RALT    | APP     | RCTRL   | FN(LED) |
+ *  '-----------------------------------------------------------------------------------------------------------------------------------------------------'
  */
-
- [_QW] = { /* QWERTY */
-  { KC_ZKHK,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  JIS_HAT,  JIS_JYEN, KC_BSPC  },
-  { KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     JIS_AT,   JIS_LBRC, XXXXXXX,  XXXXXXX  },
-  { KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  JIS_CLON, JIS_RBRC, KC_ENT,   XXXXXXX  },
-  { KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  JIS_BSLS, KC_RSFT,  XXXXXXX,  XXXXXXX  },
-  { KC_LCTL,  XXXXXXX,  KC_LWIN,  KC_LALT,  KC_MHEN,  KC_SPC,   MO(_FN),  KC_HENK,  KC_KANA,  KC_RALT,  KC_APP,   KC_RCTL,  XXXXXXX,  XXXXXXX,  MO(_LED) },
- },
-
+  [QWERTY_LAYER] = {
+    { JP_ZHTG,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     XXXXXXX,  JP_YEN,   KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     JP_MINS,  JP_CIRC },
+    { KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     XXXXXXX,  XXXXXXX,  KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     JP_AT,    JP_LBRC },
+    { KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     XXXXXXX,  KC_ESC,   KC_H,     KC_J,     KC_K,     KC_L,     JP_SCLN,  JP_COLN,  JP_RBRC },
+    { KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     XXXXXXX,  XXXXXXX,  KC_N,     KC_M,     JP_COMM,  JP_DOT,   JP_SLSH,  JP_BSLS,  KC_RSFT },
+    { KC_LCTL,  XXXXXXX,  KC_LWIN,  KC_LALT,  KC_BSPC,  KC_SPC,   JP_MHEN,  JP_HENK,  KC_ENT,   XXXXXXX,  XXXXXXX,  KC_RALT,  KC_APP,   KC_RCTL,  MO(_LED)},
+  },
 
 /* FUNCTION
  * .-----------------------------------------------------------------------------------------------------------------------------------------------------.
