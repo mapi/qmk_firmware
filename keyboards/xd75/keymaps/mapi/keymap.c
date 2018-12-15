b6f197b5658f4c162163d6ceb3a0908f64be9854
mapi <mapi@okiizo.net>
2018-12-15 22:35:19 +0900

    Add some keys

--- keyboards/xd75/keymaps/mapi/keymap.c
+++ keyboards/xd75/keymaps/mapi/keymap.c
@@ -27,20 +27,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *  .-----------------------------------------------------------------------------------------------------------------------------------------------------.
  *  | E/J     | 1       | 2       | 3       | 4       | 5       |         | \ / |   | 6       | 7       | 8       | 9       | 0       | - / =   | ^ / ~   |
  *  |---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------|
- *  | TAB     | Q       | W       | E       | R       | T       |         |         | Y       | U       | I       | O       | P       | @ / `   | [ / {   |
+ *  | TAB     | Q       | W       | E       | R       | T       |         | PrtSc   | Y       | U       | I       | O       | P       | @ / `   | [ / {   |
  *  |---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------|
- *  | LCTRL   | A       | S       | D       | F       | G       |         | ESC     | H       | J       | K       | l       | ; / +   | : / *   | ] / }   |
+ *  | LCTRL   | A       | S       | D       | F       | G       | PgUp    | ESC     | H       | J       | K       | L       | ; / +   | : / *   | ] / }   |
  *  |---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------|
- *  | LSHIFT  | Z       | X       | C       | V       | B       |         |         | N       | M       | , / <   | . / >   | / / ?   | \ / _   | RSHIFT  |
+ *  | LSHIFT  | Z       | X       | C       | V       | B       | PgDn    |         | N       | M       | , / <   | . / >   | / / ?   | \ / _   | RSHIFT  |
  *  |---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------|
  *  | LCTRL   |         | LWIN    | LALT    | BS      | SPACE   | MHEN    | HENK    | ENTER   | MO      |         | RALT    | APP     | RCTRL   | FN(LED) |
  *  '-----------------------------------------------------------------------------------------------------------------------------------------------------'
  */
   [QWERTY_LAYER] = {
     { JP_ZHTG,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     XXXXXXX,  JP_YEN,   KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     JP_MINS,  JP_CIRC },
-    { KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     XXXXXXX,  XXXXXXX,  KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     JP_AT,    JP_LBRC },
-    { KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     XXXXXXX,  KC_ESC,   KC_H,     KC_J,     KC_K,     KC_L,     JP_SCLN,  JP_COLN,  JP_RBRC },
-    { KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     XXXXXXX,  XXXXXXX,  KC_N,     KC_M,     JP_COMM,  JP_DOT,   JP_SLSH,  JP_BSLS,  KC_RSFT },
+    { KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     XXXXXXX,  KC_PSCR,  KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     JP_AT,    JP_LBRC },
+    { KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_PGUP,  KC_ESC,   KC_H,     KC_J,     KC_K,     KC_L,     JP_SCLN,  JP_COLN,  JP_RBRC },
+    { KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_PGDN,  XXXXXXX,  KC_N,     KC_M,     JP_COMM,  JP_DOT,   JP_SLSH,  JP_BSLS,  KC_RSFT },
     { KC_LCTL,  XXXXXXX,  KC_LWIN,  KC_LALT,  KC_BSPC,  KC_SPC,   JP_MHEN,  JP_HENK,  KC_ENT,   XXXXXXX,  XXXXXXX,  KC_RALT,  KC_APP,   KC_RCTL,  MO(2)   },
   },
 
