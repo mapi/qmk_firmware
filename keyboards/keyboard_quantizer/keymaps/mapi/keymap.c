8593d1ed41fd4fcdd1c24275dffc42a53e0b4cc1
mapi <mapi@okiizo.net>
2021-03-23 04:35:10 +0900

    コロン、セミコロン周りの挙動を追加

--- keyboards/keyboard_quantizer/keymaps/mapi/keymap.c
+++ keyboards/keyboard_quantizer/keymaps/mapi/keymap.c
@@ -26,6 +26,8 @@ enum layer_names {
 enum custom_keycodes {
     A_TAB = SAFE_RANGE,
     A_S_TAB,
+    MY_SCLN,
+    MY_COLN,
 };
 
 /* Original Layout(from upstream micro controller)
@@ -46,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4,     KC_F5, KC_F6, KC_F7, KC_F8,     KC_F9, KC_F10, KC_F11, KC_F12,    KC_PSCR, KC_SCROLLLOCK, KC_PAUS,
     KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, JP_MINS, JP_CIRC, JP_YEN, KC_BSPACE, KC_INSERT, KC_HOME, KC_PGUP,       KC_NUMLOCK, KC_KP_SLASH, KC_KP_ASTERISK, KC_KP_MINUS,
     KC_TAB,   KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, JP_AT, JP_LBRC,           KC_ESC,   KC_DEL, KC_END,  KC_PGDN,          KC_KP_7, KC_KP_8, KC_KP_9, KC_KP_PLUS,
-    KC_LCTRL,  KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, JP_SCLN, JP_COLN, JP_RBRC,                                                  KC_KP_4, KC_KP_5, KC_KP_6,
+    KC_LCTRL,  KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_ENT, MY_COLN, JP_RBRC,                                                  KC_KP_4, KC_KP_5, KC_KP_6,
     KC_LSFT,    KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, JP_SLSH, JP_BSLS, KC_RSFT,                 KC_UP,                     KC_KP_1, KC_KP_2, KC_KP_3, KC_KP_ENTER,
     KC_LCTRL, KC_LGUI, KC_LALT, KC_BSPACE, KC_SPC, MO(_MOD1), JP_ZKHK, KC_RALT, KC_RGUI, KC_APP, KC_RCTRL,            KC_LEFT,  KC_DOWN, KC_RIGHT,       KC_KP_0,          KC_KP_DOT
     ),
@@ -56,33 +58,86 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     XXXXXXX, LALT(KC_F4), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_HOME, KC_UP,   KC_END,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    _______, XXXXXXX, A_S_TAB, XXXXXXX, A_TAB,   XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_RIGHT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    _______, XXXXXXX, A_S_TAB, XXXXXXX, A_TAB,   XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_RIGHT, MY_SCLN, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     _______, XXXXXXX, _______, KC_DEL,  XXXXXXX, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
     )
 };
 // clang-format on
 
+
+uint8_t mod_state;
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    mod_state = get_mods();
     // A_TAB, A_S_TAB を連打した時用
     if (keycode == A_TAB) {
         if (record->event.pressed) {
             register_code(KC_LALT);
-            tap_code(KC_TAB);
+            register_code(KC_TAB);
+        } else {
+            unregister_code(KC_TAB);
         }
         return false;
     } else if (keycode == A_S_TAB) {
         if (record->event.pressed) {
             register_code(KC_LALT);
             register_code(KC_LSFT);
-            tap_code(KC_TAB);
+            register_code(KC_TAB);
+        } else {
+            unregister_code(KC_TAB);
             unregister_code(KC_LSFT);
         }
         return false;
     } else {
         unregister_code(KC_LALT);
     }
-    return true;
+
+    switch (keycode) {
+        case MY_COLN:
+            {
+            static bool my_coln_registered;
+            if (record->event.pressed) {
+                if (mod_state & MOD_MASK_SHIFT) {
+                    del_mods(MOD_MASK_SHIFT);
+                    register_code(JP_COLN);
+                    my_coln_registered = true;
+                    set_mods(mod_state);
+                } else {
+                    register_code(JP_SCLN);
+                }
+            } else {
+                if (my_coln_registered) {
+                    unregister_code(JP_COLN);
+                    my_coln_registered = false;
+                } else {
+                    unregister_code(JP_SCLN);
+                }
+            }
+            return false;
+        }
+        case MY_SCLN:
+            {
+            static bool my_scln_registered;
+            if (record->event.pressed) {
+                if (mod_state & MOD_MASK_SHIFT) {
+                    register_code16(LSFT(JP_SCLN));
+                    my_scln_registered = true;
+                } else {
+                    register_code16(LSFT(JP_COLN));
+                }
+            } else {
+                if (my_scln_registered) {
+                    unregister_code16(LSFT(JP_SCLN));
+                    my_scln_registered = false;
+                } else {
+                    unregister_code16(LSFT(JP_COLN));
+                }
+            }
+            return false;
+        }
+        default:
+            return true;
+    }
 };
 
 #ifdef OLED_DRIVER_ENABLE
@@ -93,4 +148,3 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 #ifdef OLED_DRIVER_ENABLE
 void oled_task_user(void) { render_logo(); }
 #endif
-
