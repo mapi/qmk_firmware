dccd2e5fba29a10bdccc165fef504f8893dcd710
mapi <mapi@okiizo.net>
2024-01-04 09:46:10 +0900

    shift number and function key.

--- keyboards/keebio/bfo9000/keymaps/mapi/keymap.c
+++ keyboards/keebio/bfo9000/keymaps/mapi/keymap.c
@@ -127,8 +127,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_BASE] = LAYOUT( \
-    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,  XXXXXXX,   XXXXXXX, XXXXXXX,    XXXXXXX, KC_F6,  KC_F7,     KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_BSPC, \
-    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   XXXXXXX,   XXXXXXX, XXXXXXX,    XXXXXXX, KC_6,   KC_7,      KC_8,    KC_9,    KC_0,    JP_MINS, JP_CIRC, JP_YEN, \
+    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,  KC_F6,     XXXXXXX, XXXXXXX,    XXXXXXX, KC_F7,  KC_F8,     KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_BSPC, XXXXXXX, \
+    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6,      XXXXXXX, XXXXXXX,    XXXXXXX, KC_7,   KC_8,      KC_9,    KC_0,    JP_MINS, JP_CIRC, JP_YEN,  XXXXXXX, \
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   XXXXXXX,   XXXXXXX, XXXXXXX,    XXXXXXX, KC_Y,   KC_U,      KC_I,    KC_O,    KC_P,    JP_AT,   JP_LBRC, XXXXXXX, \
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_WBAK,   KC_WFWD, XXXXXXX,    XXXXXXX, KC_H,   KC_J,      KC_K,    KC_L,    KC_ENT,  CK1,     JP_RBRC, KC_ESC, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   XXXXXXX,   XXXXXXX, XXXXXXX,    XXXXXXX, KC_N,   KC_M,      JP_COMM, JP_DOT,  JP_SLSH, JP_BSLS, KC_UP,   XXXXXXX, \
